190096cd9776c21998336a899b0a1b9ffbed87e6
J03B <byucrazyfan@gmail.com>
2022-12-12 18:34:32 -0600

    Added needed variables for generatePassword and length verification

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,33 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// ADDING CODE BELOW TO GENERATE THE PASSWORD
+function generatePassword() {
+    let pwLength;
+    let validLength = false;
+    let pwCharType = [0, 0, 0, 0];
+    let sumCharType = 0;
+    let validCharType = [false, false, false, false];
+    let charTypeCriteria = ["lowercase", "uppercase", "numeric", "special"];
+    let pwText = "";
+    
+    // First, prompt for the length of the password
+    while (!validLength) {
+      pwLength = window.prompt("Enter the length of the password");
+  
+      // validate the length is an integer between 8 and 128
+      if (pwLength % 1 == 0) {
+        if (pwLength >= 8 && pwLength <= 128) {
+          validLength = true;
+        }
+        else {
+          window.alert("You must enter a value between 8 and 128.");
+        }
+      }
+      else {
+        window.alert("You must enter an integer (whole number). This will be the length of the password.");
+      } 
+    }
+    
+}
